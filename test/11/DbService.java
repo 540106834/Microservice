8a6912d21103961e358195c2d908d847e3e2fd78
540106834 <540106834@qq.com>
2026-01-31 21:43:23 +0900

    11

---
+++ test/11/DbService.java
@@ -0,0 +1,6 @@
+public class DbService extends Service {
+    @Override
+    public void start() {
+        System.out.println("启动 数据库 服务");
+    }
+}
---
+++ test/11/WebService.java
@@ -0,0 +1,6 @@
+class WebService extends Service {
+    @Override
+    public void start() {
+        System.out.println("启动 Web 服务");
+    }
+}
